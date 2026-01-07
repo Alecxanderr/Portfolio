a30bfaf7aa3ad6ee4a520e0132473912488dc167
Alecxander Cooper <xandcooper@gmail.com>
2026-01-06 16:49:18 -0800

    created pages, set up css

--- frontend/myPortfolio/vite.config.js
+++ frontend/myPortfolio/vite.config.js
@@ -6,10 +6,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //setting an alias, makes it easier to import from components like app.jsx
   resolve: {
     alias: {
-      "@": 
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
-// setting an alias
